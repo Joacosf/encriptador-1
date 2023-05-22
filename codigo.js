894ba0d6dae5da53d4d0aba27737dd2bf6b2e893
Joacosf <joacosf12@gmail.com>
2023-05-21 21:04:34 -0300

    trabajo en js

--- codigo.js
+++ codigo.js
@@ -1,30 +1,31 @@
 const btnEncriptar = document.querySelector(".btnEncriptar");
 const btnDesencriptar = document.querySelector(".btnDesencriptar");
-const inputText = document.querySelector(".inputText");
 const texto = document.getElementById("texto");
 
-
 btnEncriptar.addEventListener("click", ()=> {
+    
+    const inputText = document.querySelector(".input").value.toLowerCase();
 
-    inputText.value.forEach(letra => {
-        switch(letra){
-            case "a":
-                letra = "aix";
-                break;
-            case "b":
-                letra = "eloy";
-                break;
-            case "i":
-                letra = "inti";
-                break;
-            case "o":
-                letra = "over";
-                break;
-            case "u":
-                letra = "ulloa";
-                break;
-            default:
-        }
-        console.log(inputText);
-    });
+    txt = inputText.replace(/a/img,"airx");
+    var txt = txt.replace(/e/img,"elvus");
+    var txt = txt.replace(/i/img,"inti");
+    var txt = txt.replace(/o/img,"over");
+    var txt = txt.replace(/u/img,"under");
+
+    document.querySelector(".input").textContent = "mensaje encriptado";
+    texto.innerHTML = txt;
+});
+
+btnDesencriptar.addEventListener("click",() =>{
+
+    const inputText = document.querySelector(".input").value.toLowerCase();
+
+    var txt = inputText.replace(/airx/img,"a");
+    var txt = txt.replace(/elvus/img,"e");
+    var txt = txt.replace(/inti/img,"i");
+    var txt = txt.replace(/over/img,"o");
+    var txt = txt.replace(/under/img,"u");
+
+    texto.innerHTML = txt;
+
 });
